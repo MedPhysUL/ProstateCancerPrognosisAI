3f24cde49b4625a834f3b89c56c3acbcf8d660c8
Raphaël Brodeur <106177338+raphbrodeur@users.noreply.github.com>
2022-06-08 17:25:14 -0400

    Update visualizer.py

--- src/visualization/visualizer.py
+++ src/visualization/visualizer.py
@@ -5,11 +5,13 @@
     @Creation Date:     05/2022
     @Last modification: 05/2022
 
-    @Description:       Description.
-
+    @Description:       This file contains the Visualizer class which is used to visualize a patient's image (whether
+                        it is a PET or a CT) alongside its respective segmentation. Sliders allow the user to slice
+                        through the coronal, sagittal and axial views.
 """
 
 from enum import IntEnum
+from functools import partial
 from typing import Dict, List, Union, Tuple
 
 import numpy as np
@@ -18,192 +20,152 @@ from matplotlib.widgets import Slider
 
 
 class AnatomicalPlane(IntEnum):
-    ALL = -1
-    AXIAL = 0
-    CORONAL = 1
-    SAGITTAL = 2
+    ALL = -1,
+    CORONAL = 0,
+    SAGITTAL = 1,
+    AXIAL = 2
 
 
 class Visualizer:
     """
-
+    A class which is used to visualize a patient's image (whether it is a PET or a CT) alongside its respective
+    segmentation. Sliders allow the user to slice through the coronal, sagittal and axial views.
     """
 
-    def _visualize_axial():
-        pass
-    def _visualize_coronal():
-        pass
-    def _
     @staticmethod
-    def visualize(
+    def _visualize_plane(
+            plane: AnatomicalPlane,
             img: np.ndarray,
-            seg: np.ndarray
-    ) -> Tuple[plt.figure, plt.axes]:
+            seg: np.ndarray,
+            axes: np.ndarray
+    ) -> Dict[str, Union[np.ndarray, Slider, plt.axes]]:
         """
-        Create a 3D figure of a given image array in all anatomical plane views.
+        Initializes the figures.
+
+        Parameters
+        ----------
+        plane : AnatomicalPlane
+            The anatomical plane to visualize. Coronal, sagittal or axial.
+        img : np.ndarray
+            An image array.
+        seg : np.ndarray
+            A segmentation map array.
+        axes : plt.axes
+            Axes.
+
+        Returns
+        -------
+        returns : Dict[np.ndarray, np.ndarray, Slider, plt.axes, plt.axes]
+            A dictionary pertaining to visualization.
+        """
+        max_slice = img.shape[plane.value] - 1
+        img_max = np.max(img)
+        img_min = np.min(img)
+
+        img_plane_in_first_axis = np.moveaxis(img, plane.value, 0)
+        initial_img = img_plane_in_first_axis[int(max_slice / 2)]
+        seg_plane_in_first_axis = np.moveaxis(seg, plane.value, 0)
+        initial_seg = seg_plane_in_first_axis[int(max_slice / 2)]
+
+        img_3d = axes[plane.value].imshow(
+            initial_img,
+            cmap="Greys_r",
+            vmax=img_max,
+            vmin=img_min
+        )
+        seg_3d = axes[plane.value].imshow(
+            initial_seg,
+            vmax=1,
+            vmin=0,
+            alpha=0.1
+        )
+
+        ax_slice = plt.axes(
+            [0.14 + plane.value * 0.27, 0.15, 0.2, 0.02],
+            facecolor='lightgoldenrodyellow'
+        )
+        slice_index = Slider(
+            ax_slice,
+            'Slice',
+            0,
+            max_slice,
+            valinit=int(max_slice / 2),
+            valstep=1
+        )
+
+        returns = {
+            "img_plane_in_first_axis": img_plane_in_first_axis,
+            "seg_plane_in_first_axis": seg_plane_in_first_axis,
+            "slice_index": slice_index,
+            "img_3d": img_3d,
+            "seg_3d": seg_3d
+        }
+
+        return returns
+
+    @staticmethod
+    def _update_plane(
+            val,
+            plane: AnatomicalPlane,
+            set_up: Dict[str, Union[np.ndarray, Slider, plt.axes]]
+    ):
+        """
+        Updates the figure associated to a slider.
+
+        Parameters
+        ----------
+        plane : AnatomicalPlane
+            The anatomical plane to update. Coronal, sagittal or axial.
+        set_up : Dict[str, Union[np.ndarray, Slider, plt.axes]]
+            A dictionary output by the _visualize_plane method.
+        """
+        slice_value = set_up["slice_index"].val
+        new_img = set_up["img_plane_in_first_axis"][slice_value]
+        new_seg = set_up["seg_plane_in_first_axis"][slice_value]
+        set_up["img_3d"].set_data(new_img)
+        set_up["seg_3d"].set_data(new_seg)
+        plt.draw()
+
+    def visualize(
+            self,
+            img,
+            seg
+    ):
+        """
+        Visualizes a patient's image (whether it is a PET or a CT) alongside its respective segmentation. Sliders allow
+        the user to slice through the coronal, sagittal and axial views.
 
         Parameters
         ----------
         img : np.ndarray
             An image array.
         seg : np.ndarray
-            A segmentation mask array.
-
-        Returns
-        -------
-        fig, axes : Tuple[plt.figure, plt.axes]
-            Figure and axes.
+            A segmentation map array.
         """
         fig, axes = plt.subplots(1, 3)
         plt.subplots_adjust(bottom=0.20)
-        img_max = np.max(img)
-        img_min = np.min(img)
 
+        set_up_coronal = self._visualize_plane(plane=AnatomicalPlane.CORONAL, img=img, seg=seg, axes=axes)
+        set_up_sagittal = self._visualize_plane(plane=AnatomicalPlane.SAGITTAL, img=img, seg=seg, axes=axes)
+        set_up_axial = self._visualize_plane(plane=AnatomicalPlane.AXIAL, img=img, seg=seg, axes=axes)
 
-        # Axial
-        img_with_axial_plane_moved_to_first_axis = np.moveaxis(img, AnatomicalPlane.AXIAL, 0)
-        max_axial_slice = img.shape[AnatomicalPlane.AXIAL] - 1
-        initial_axial_img = img_with_axial_plane_moved_to_first_axis[int(max_axial_slice / 2)]
-        seg_with_axial_plane_moved_to_first_axis = np.moveaxis(seg, AnatomicalPlane.AXIAL, 0)
-        initial_axial_seg = seg_with_axial_plane_moved_to_first_axis[int(max_axial_slice / 2)]
+        set_up_coronal["slice_index"].on_changed(partial(
+            self._update_plane,
+            plane=AnatomicalPlane.CORONAL,
+            set_up=set_up_coronal
+        ))
+        set_up_sagittal["slice_index"].on_changed(partial(
+            self._update_plane,
+            plane=AnatomicalPlane.SAGITTAL,
+            set_up=set_up_sagittal
+        ))
+        set_up_axial["slice_index"].on_changed(partial(
+            self._update_plane,
+            plane=AnatomicalPlane.AXIAL,
+            set_up=set_up_axial
+        ))
 
-        axial_3d_img = axes[AnatomicalPlane.AXIAL].imshow(
-            initial_axial_img,
-            cmap="Greys_r",
-            vmax=img_max,
-            vmin=img_min
-        )
-
-        axial_3d_seg = axes[AnatomicalPlane.AXIAL].imshow(
-            initial_axial_seg,
-            vmax=1,
-            vmin=0,
-            alpha=0.1
-        )
-
-        ax_slice = plt.axes(
-            [0.14 + AnatomicalPlane.AXIAL.value * 0.27, 0.15, 0.2, 0.02],
-            facecolor='lightgoldenrodyellow'
-        )
-
-        axial_slice_index = Slider(
-            ax_slice,
-            'Slice',
-            0,
-            max_axial_slice,
-            valinit=int(max_axial_slice / 2),
-            valstep=1
-        )
-
-        def axial_image_update(val):
-            slice_value = axial_slice_index.val
-            new_img = img_with_axial_plane_moved_to_first_axis[slice_value]
-            new_seg = seg_with_axial_plane_moved_to_first_axis[slice_value]
-            axial_3d_img.set_data(new_img)
-            axial_3d_seg.set_data(new_seg)
-            plt.draw()
-
-        axial_slice_index.on_changed(axial_image_update)
-
-        # Coronal
-        img_with_coronal_plane_moved_to_first_axis = np.moveaxis(img, AnatomicalPlane.CORONAL, 0)
-        max_coronal_slice = img.shape[AnatomicalPlane.CORONAL] - 1
-        initial_coronal_img = img_with_coronal_plane_moved_to_first_axis[int(max_coronal_slice / 2)]
-
-        seg_with_coronal_plane_moved_to_first_axis = np.moveaxis(seg, AnatomicalPlane.CORONAL, 0)
-        initial_coronal_seg = seg_with_coronal_plane_moved_to_first_axis[int(max_coronal_slice / 2)]
-
-        coronal_3d_img = axes[AnatomicalPlane.CORONAL].imshow(
-            initial_coronal_img,
-            cmap="Greys_r",
-            vmax=img_max,
-            vmin=img_min
-        )
-
-        coronal_3d_seg = axes[AnatomicalPlane.CORONAL].imshow(
-            initial_coronal_seg,
-            vmax=1,
-            vmin=0,
-            alpha=0.1
-        )
-
-        ax_slice = plt.axes(
-            [0.14 + AnatomicalPlane.CORONAL.value * 0.27, 0.15, 0.2, 0.02],
-            facecolor='lightgoldenrodyellow'
-        )
-
-        coronal_slice_index = Slider(
-            ax_slice,
-            'Slice',
-            0,
-            max_coronal_slice,
-            valinit=int(max_coronal_slice / 2),
-            valstep=1
-        )
-
-        def coronal_image_update(val):
-            slice_value = coronal_slice_index.val
-            new_img = img_with_coronal_plane_moved_to_first_axis[slice_value]
-            new_seg = seg_with_coronal_plane_moved_to_first_axis[slice_value]
-            coronal_3d_img.set_data(new_img)
-            coronal_3d_seg.set_data(new_seg)
-            plt.draw()
-
-        coronal_slice_index.on_changed(coronal_image_update)
-
-        # Sagittal
-        img_with_sagittal_plane_moved_to_first_axis = np.moveaxis(img, AnatomicalPlane.SAGITTAL, 0)
-
-        max_sagittal_slice = img.shape[AnatomicalPlane.SAGITTAL] - 1
-
-        initial_sagittal_img = img_with_sagittal_plane_moved_to_first_axis[int(max_sagittal_slice / 2)]
-
-        seg_with_sagittal_plane_moved_to_first_axis = np.moveaxis(seg, AnatomicalPlane.SAGITTAL, 0)
-
-        initial_sagittal_seg = seg_with_sagittal_plane_moved_to_first_axis[int(max_sagittal_slice / 2)]
-
-        sagittal_3d_img = axes[AnatomicalPlane.SAGITTAL].imshow(
-            initial_sagittal_img,
-            cmap="Greys_r",
-            vmax=img_max,
-            vmin=img_min
-
-        )
-
-        sagittal_3d_seg = axes[AnatomicalPlane.SAGITTAL].imshow(
-            initial_sagittal_seg,
-            vmax=1,
-            vmin=0,
-            alpha=0.1
-        )
-
-        ax_slice = plt.axes(
-            [0.14 + AnatomicalPlane.SAGITTAL.value * 0.27, 0.15, 0.2, 0.02],
-            facecolor='lightgoldenrodyellow'
-        )
-
-        sagittal_slice_index = Slider(
-            ax_slice,
-            'Slice',
-            0,
-            max_sagittal_slice,
-            valinit=int(max_sagittal_slice / 2),
-            valstep=1
-        )
-
-        def sagittal_image_update(val):
-            slice_value = sagittal_slice_index.val
-            new_img = img_with_sagittal_plane_moved_to_first_axis[slice_value]
-            new_seg = seg_with_sagittal_plane_moved_to_first_axis[slice_value]
-            sagittal_3d_img.set_data(new_img)
-            sagittal_3d_seg.set_data(new_seg)
-            plt.draw()
-
-        sagittal_slice_index.on_changed(sagittal_image_update)
-        
         plt.show()
-        return fig, axes
-
 
 
 
