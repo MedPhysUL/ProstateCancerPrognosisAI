5c7157eaf12345b950e7cea563535a1250fdf64e
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-06-10 17:20:41 -0400

    Create local extractor

---
+++ applications/create_local_database.py
@@ -0,0 +1,32 @@
+"""
+    @file:              create_local_database.py
+    @Author:            Maxence Larose
+    @Creation Date:     06/2022
+    @Last modification: 06/2022
+    @Description:       This file contains the LocalExtractor class which is used to create an HDF5 database containing
+                        the images and their segmentations.
+"""
+
+import env_apps
+
+from dicom2hdf import transforms
+
+from src.data.extraction.local import LocalExtractor
+
+
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                               Logs Setup                                                    #
+    # ----------------------------------------------------------------------------------------------------------- #
+    env_apps.configure_logging("logging_conf.yaml")
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                             Create Database                                                 #
+    # ----------------------------------------------------------------------------------------------------------- #
+    extractor = LocalExtractor(path_to_database=r"local_data/patients_database.h5")
+
+    patients_who_failed = extractor.create_database(
+        path_to_patients_folder=r"local_data/Patients",
+        series_descriptions=r"local_data/series_descriptions.json",
+        transformations=[transforms.Resample((1.5, 1.5, 1.5))]
+    )
---
+++ src/data/extraction/local.py
@@ -0,0 +1,78 @@
+"""
+    @file:              local.py
+    @Author:            Maxence Larose
+
+    @Creation Date:     06/2022
+    @Last modification: 06/2022
+
+    @Description:       This file contains the LocalExtractor class which is used to create HDF5 files containing the
+                        images and their segmentations.
+"""
+
+from typing import Dict, List, Optional, Sequence, Union
+
+from dicom2hdf import PatientsDataset, PatientWhoFailed
+from dicom2hdf.processing.transforms import BaseTransform
+
+
+class LocalExtractor:
+    """
+    A LocalExtractor class which is used to extract images contained in DICOM files and aggregate these images and some
+    metadata into an HDF5 file. This file is easier to parse than the original DICOM files to retrieve images and
+    segmentations.
+    """
+
+    def __init__(self, path_to_database: str):
+        """
+        Initializes the dataset.
+
+        Parameters
+        ----------
+        path_to_database : str
+            Path to dataset.
+        """
+        self.database = PatientsDataset(
+            path_to_dataset=path_to_database
+        )
+
+    def create_database(
+            self,
+            path_to_patients_folder,
+            series_descriptions: Optional[Union[str, Dict[str, List[str]]]],
+            transformations: Optional[Sequence[BaseTransform]],
+            overwrite_dataset: bool = False
+    ) -> List[PatientWhoFailed]:
+        """
+        Creates an HDF dataset from multiple patients DICOM files.
+
+        Parameters
+        ----------
+        path_to_patients_folder : str
+            The path to the folder that contains all the patients' folders.
+        series_descriptions : Optional[Union[str, Dict[str, List[str]]]].
+            A dictionary that contains the series descriptions of the images that needs to be extracted from the
+            patient's file. Keys are arbitrary names given to the images we want to add and values are lists of
+            series descriptions. The images associated with these series descriptions do not need to have a
+            corresponding segmentation. Note that it can be specified as a path to a json dictionary that contains the
+            series descriptions.
+        transformations : Optional[Sequence[BaseTransform]]
+            A sequence of transformations to apply to images and segmentations.
+        overwrite_dataset : bool, default = False.
+            Overwrite existing dataset.
+
+        Returns
+        -------
+        patients_who_failed : List[PatientWhoFailed]
+            List of patients with one or more images not added to the HDF5 dataset due to the absence of the series in
+            the patient record.
+        """
+        patients_who_failed = self.database.create_hdf5_dataset(
+            path_to_patients_folder=path_to_patients_folder,
+            tags_to_use_as_attributes=[(0x0008, 0x103E), (0x0020, 0x000E), (0x0008, 0x0060)],
+            series_descriptions=series_descriptions,
+            transforms=transformations,
+            erase_unused_dicom_files=True,
+            overwrite_dataset=overwrite_dataset
+        )
+
+        return patients_who_failed
