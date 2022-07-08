42b0474a030bcbee6a341d16c0340ced9bc43c57
Raphaël Brodeur <106177338+raphbrodeur@users.noreply.github.com>
2022-07-08 11:23:30 -0400

    Update create_local_database.py

--- applications/create_local_database.py
+++ applications/create_local_database.py
@@ -23,10 +23,10 @@ if __name__ == "__main__":
     # ----------------------------------------------------------------------------------------------------------- #
     #                                             Create Database                                                 #
     # ----------------------------------------------------------------------------------------------------------- #
-    extractor = LocalExtractor(path_to_database=r"local_data/patients_database.h5")
+    extractor = LocalExtractor(path_to_database=r"local_data/learning_set.h5")
 
     patients_who_failed = extractor.create_database(
-        path_to_patients_folder=r"local_data/Patients",
+        path_to_patients_folder=r"local_data/Learning_set",
         series_descriptions=r"local_data/series_descriptions.json",
         transformations=[transforms.Resample((1.5, 1.5, 1.5))]
     )
