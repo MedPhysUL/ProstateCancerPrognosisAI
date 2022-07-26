da799f2dd75a8f8f83ee153ba68a31f3f20f5000
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-07-26 15:24:38 -0400

    Refractoring extractor to manager

--- applications/create_database.py
+++ applications/create_database.py
@@ -1,17 +1,18 @@
 """
     @file:              create_local_database.py
     @Author:            Maxence Larose
+
     @Creation Date:     06/2022
-    @Last modification: 06/2022
-    @Description:       This file contains the LocalExtractor class which is used to create an HDF5 database containing
-                        the images and their segmentations.
+    @Last modification: 07/2022
+
+    @Description:       A small script that shows how to create an HDF5 file database using a LocalDatabaseManager.
 """
 
 import env_apps
 
 from dicom2hdf import transforms
 
-from src.data.extraction.local import LocalExtractor
+from src.data.extraction.local import LocalDatabaseManager
 
 
 if __name__ == "__main__":
@@ -23,9 +24,9 @@ if __name__ == "__main__":
     # ----------------------------------------------------------------------------------------------------------- #
     #                                             Create Database                                                 #
     # ----------------------------------------------------------------------------------------------------------- #
-    extractor = LocalExtractor(path_to_database=r"local_data/learning_set.h5")
+    database_manager = LocalDatabaseManager(path_to_database=r"local_data/learning_set.h5")
 
-    patients_who_failed = extractor.create_database(
+    database_manager.create_database(
         path_to_patients_folder=r"local_data/Learning_set",
         series_descriptions=r"local_data/series_descriptions.json",
         transformations=[transforms.Resample((1.5, 1.5, 1.5))],
