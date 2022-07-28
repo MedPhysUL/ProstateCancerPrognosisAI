ed4a47f8e21535d92f161c8b69f49f7c28e58e0d
Maxence Larose <maxence.larose.1@ulaval.ca>
2022-07-28 12:42:48 -0400

    Switch None for NaN to represent unknown values in dataset

--- src/data/processing/prostate_cancer_dataset.py
+++ src/data/processing/prostate_cancer_dataset.py
@@ -11,11 +11,26 @@
 from typing import List, NamedTuple, Optional, Tuple, Union
 
 import numpy as np
-from torch import tensor
+from torch import nan, tensor
 from torch.utils.data import Dataset
 
-from image_dataset import ImageDataset
-from multi_task_table_dataset import MultiTaskTableDataset
+from src.data.processing.image_dataset import ImageDataset
+from src.data.processing.multi_task_table_dataset import MultiTaskTableDataset
+
+
+class EmptyDataset(Dataset):
+
+    def __init__(
+            self,
+            to_tensor: bool = True
+    ):
+        self.to_tensor = to_tensor
+
+    def __getitem__(self, index):
+        if self.to_tensor:
+            return nan
+        else:
+            return np.nan
 
 
 class DataItem(NamedTuple):
@@ -31,21 +46,40 @@ class ProstateCancerDataset(Dataset):
 
     def __init__(
             self,
-            image_dataset: ImageDataset,
-            table_dataset: MultiTaskTableDataset
+            image_dataset: Optional[ImageDataset] = None,
+            table_dataset: Optional[MultiTaskTableDataset] = None
     ):
         """
         Sets protected and public attributes of our custom dataset class.
 
         Parameters
         ----------
-        image_dataset : ImageDataset
+        image_dataset : Optional[ImageDataset]
             An ImageDataset.
-        table_dataset : MultiTaskTableDataset
+        table_dataset : Optional[MultiTaskTableDataset]
             A MultiTaskTableDataset.
         """
-        self.image_dataset = image_dataset
-        self.table_dataset = table_dataset
+        if (image_dataset is None) and (table_dataset is None):
+            raise AssertionError("At least one image dataset or one table dataset must be provided.")
+
+        if table_dataset is None:
+            self.table_dataset = EmptyDataset()
+            self.image_dataset = image_dataset
+            self._length = len(image_dataset)
+        elif image_dataset is None:
+            self.table_dataset = table_dataset
+            self.image_dataset = EmptyDataset(to_tensor=table_dataset.to_tensor)
+            self._length = len(table_dataset)
+        else:
+            tab_length, img_length = len(table_dataset), len(image_dataset)
+            assert tab_length == img_length, f"Length of image dataset and table dataset must be equal. Image length " \
+                                             f": {img_length}. Table length : {tab_length}."
+            self.table_dataset = table_dataset
+            self.image_dataset = image_dataset
+            self._length = len(image_dataset)
+
+    def __len__(self):
+        return self._length
 
     def __getitem__(
             self,
