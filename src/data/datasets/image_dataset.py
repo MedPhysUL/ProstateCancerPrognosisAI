b57ab5def033535894a63e5898788c228c3c4ec3
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-11-17 16:48:20 -0500

    Image dataset directly uses delia

--- src/data/datasets/image_dataset.py
+++ src/data/datasets/image_dataset.py
@@ -11,13 +11,13 @@
 """
 
 import collections.abc
-from typing import Callable, Dict, List, NamedTuple, Optional, Union, Sequence, Set, Tuple
+from typing import Callable, List, NamedTuple, Optional, Union, Sequence, Set
 
-from monai.transforms import apply_transform, CropForeground, SpatialCrop
+from delia.databases.patients_database import PatientsDatabase
+from monai.transforms import apply_transform
 import numpy as np
 from torch.utils.data import Dataset, Subset
 
-from src.data.extraction.local import LocalDatabaseManager
 from src.utils.tasks import SegmentationTask
 
 
@@ -36,11 +36,10 @@ class ImageDataset(Dataset):
 
     def __init__(
             self,
-            database_manager: LocalDatabaseManager,
+            database: PatientsDatabase,
             tasks: List[SegmentationTask],
             modalities: Set[str],
-            transforms: Optional[Callable] = None,
-            z_dim: ZDimension = ZDimension(start=50, stop=210)
+            transforms: Optional[Callable] = None
     ) -> None:
         """
         Creates a dataset of various patients and their respective CT and segmentation map from a given local HDf5 file.
@@ -48,8 +47,8 @@ class ImageDataset(Dataset):
 
         Parameters
         ----------
-        database_manager : LocalDatabaseManager
-            A database manager that is used to interact with the HDF5 file that contains all the patients' folders.
+        database : PatientsDatabase
+            A database that is used to interact with the HDF5 file that contains all the patients' folders.
         tasks : List[SegmentationTask]
             Task to perform.
         transforms : Optional[Callable]
@@ -57,15 +56,13 @@ class ImageDataset(Dataset):
         z_dim : ZDimension
             A tuple that specify the z-dimension crop.
         """
-        self._db_manager = database_manager
+        self._db = database
         self._modalities = modalities
         self._tasks = tasks
         self._transforms = transforms
-        self._z_dim = z_dim
 
     def __len__(self) -> int:
-        db = self._db_manager.get_database()
-        return len(db.keys())
+        return len(self._db)
 
     def __getitem__(self, index: Union[int, slice, Sequence[int]]):
         if isinstance(index, slice):
@@ -80,26 +77,19 @@ class ImageDataset(Dataset):
         return self._transform(data_i)
 
     def _get_single_patient(self, index: int):
-        db = self._db_manager.get_database()
-        patient = list(db.keys())[index]
+        patient_group = self._db[index]
 
-        print(f"Loading {patient}.")
+        print(f"Loading {patient_group.name[1:]}.")
         img_dict, seg_dict = {}, {}
-        for series_number in db[patient].keys():
-            series = db[patient][series_number]
+        for series_number in patient_group.keys():
+            series = patient_group[series_number]
             for modality in self._modalities:
-                if series.attrs[self._db_manager.MODALITY] == modality:
-                    img_dict[modality] = self._transpose(
-                        data=series[self._db_manager.IMAGE]
-                    ).astype(np.float16)
+                if series.attrs[self._db.MODALITY] == modality:
+                    img_dict[modality] = self._transpose(data=series[self._db.IMAGE]).astype(np.float16)
 
                     for task in self._tasks:
                         if modality == task.modality:
-                            seg_dict[task.organ] = self._transpose(
-                                data=series["0"][f"{task.organ}_label_map"]
-                            ).astype(np.int8)
-
-        img_dict, seg_dict = self._crop(img_dict=img_dict, seg_dict=seg_dict, z_dim=self._z_dim)
+                            seg_dict[task.organ] = self._transpose(data=series["0"][task.organ]).astype(np.int8)
 
         return dict(img_dict, **seg_dict)
 
@@ -113,44 +103,3 @@ class ImageDataset(Dataset):
     @staticmethod
     def _transpose(data) -> np.array:
         return np.transpose(np.array(data), (2, 0, 1))
-
-    @staticmethod
-    def _crop(
-            img_dict: Dict[str, np.array],
-            seg_dict: Dict[str, np.array],
-            z_dim: ZDimension = None
-    ) -> Tuple[Dict[str, np.ndarray], Dict[str, np.ndarray]]:
-        """
-        Crops the foreground. A crop along Z can also be specified.
-
-        Parameters
-        ----------
-         img_dict: Dict[str, np.array]
-            An image array in shape (Z, X, Y).
-        seg_dict: Dict[str, np.array]
-            A segmentation map array in shape (Z, X, Y).
-        z_dim : ZDimension
-            Lower bound and upper bound of the crop to apply along Z.
-
-        Returns
-        -------
-        img_cropped, seg_cropped : Tuple[np.ndarray, np.ndarray]
-            A cropped image array and a cropped segmentation map array.
-        """
-        img_dict["CT"], start, end = CropForeground(return_coords=True)(img_dict["CT"])
-
-        for name, seg in seg_dict.items():
-            seg_dict[name] = SpatialCrop(roi_start=start, roi_end=end)(seg)
-
-        for name, img in img_dict.items():
-            if name != "CT":
-                img_dict[name] = SpatialCrop(roi_start=start, roi_end=end)(img)
-
-        if z_dim:
-            for name, img in img_dict.items():
-                img_dict[name] = img[z_dim[0]: z_dim[1]]
-
-            for name, seg in seg_dict.items():
-                seg_dict[name] = seg[z_dim[0]: z_dim[1], :, :]
-
-        return img_dict, seg_dict
