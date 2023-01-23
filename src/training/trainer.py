80c34a18622829fd788c759a8ef5393643de5fb7
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-01-23 15:13:09 -0500

    Update trainer.py

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,94 +1,20 @@
-
-from dataclasses import dataclass, field
-from typing import Any, Dict, Generator, List, Optional, Union
+from typing import Generator, List, Optional, Union
 
 from monai.data import DataLoader
-import numpy as np
-from torch import cuda, device, no_grad, Tensor
+from torch import device as torch_device
+from torch import cuda, no_grad, Tensor
 from torch.utils.data import SubsetRandomSampler
 from tqdm.auto import tqdm
 
 from src.callbacks.callback import Callback
 from src.callbacks.callback_list import CallbackList
 from src.callbacks.model_checkpoint import CheckpointLoadingMode, ModelCheckpoint
-from src.callbacks.training_history import MeasureCategory, TrainingHistory
+from src.callbacks.training_history import TrainingHistory
+from src.callbacks.learning_algorithm import LearningAlgorithm
 from src.data.datasets.prostate_cancer_dataset import FeaturesModel, ProstateCancerDataset
-from src.data.processing.tools import MaskType
-# TODO : Move MaskType elsewhere and (Transform Masktype in Enum or MeasureCategory in a classic class).
 from src.models.base.base_model import BaseModel
-from src.utils.transforms import to_numpy, ToTensor
-
-
-@dataclass
-class TrainingState:
-    """
-    This class is used to store the current training state. It is extremely useful for the callbacks to access the
-    current training state and to modify the training process.
-
-    Elements
-    --------
-    batch : int
-        The current batch.
-    batch_loss : float
-        The current loss.
-    epoch : int
-        The current epoch.
-    epoch_losses_and_metrics : Dict[str, Dict[str, Dict[str, float]]]
-        The current epoch losses and metrics, i.e the current training set losses and metrics and validation set
-        losses and metrics.
-    info : Dict[str, Any]
-        Any additional information. This is useful to communicate between callbacks.
-    n_epochs : int
-        Total number of epochs.
-    objects : Dict[str, Any]
-        Any additional objects. This is useful to manage objects between callbacks. Note: In general, the
-        train_dataloader, valid_dataloader and tasks objetcs should be stored here.
-    pred_batch : DataModel.y
-        The current prediction.
-    stop_training_flag : bool
-        Whether the training should be stopped.
-    x_batch : DataModel.x
-        The current input batch.
-    y_batch : DataModel.y
-        The current target batch.
-    """
-    batch: Optional[int] = None
-    batch_loss: Optional[float] = None
-    epoch: Optional[int] = None
-    epoch_losses_and_metrics: Dict[str, Dict[str, Dict[str, float]]] = field(default_factory=dict)
-    info: Dict[str, Any] = field(default_factory=dict)
-    n_epochs: Optional[int] = None
-    objects: Dict[str, Any] = field(default_factory=dict)
-    pred_batch: Optional[Dict[str, Tensor]] = None
-    stop_training_flag: bool = False
-    x_batch: Optional[FeaturesModel] = None
-    y_batch: Optional[Dict[str, Tensor]] = None
-
-    def __getstate__(self):
-        unserializable = ["info", "objects"]
-        d = {k: v for k, v in vars(self).items() if k not in unserializable}
-        return d
-
-    def update(self, **kwargs):
-        self_dict = vars(self)
-        assert all(k in self_dict for k in kwargs)
-        self_dict.update(kwargs)
-
-    @property
-    def train_losses(self) -> Dict[str, float]:
-        return self.epoch_losses_and_metrics[MaskType.TRAIN][MeasureCategory.LOSSES.value]
-
-    @property
-    def train_metrics(self) -> Dict[str, float]:
-        return self.epoch_losses_and_metrics[MaskType.TRAIN][MeasureCategory.METRICS.value]
-
-    @property
-    def valid_losses(self) -> Dict[str, float]:
-        return self.epoch_losses_and_metrics[MaskType.VALID][MeasureCategory.LOSSES.value]
-
-    @property
-    def valid_metrics(self) -> Dict[str, float]:
-        return self.epoch_losses_and_metrics[MaskType.VALID][MeasureCategory.METRICS.value]
+from src.training.states import BatchState, BatchesState, EpochState, TrainingState
+from src.utils.transforms import ToTensor
 
 
 class Trainer:
@@ -96,7 +22,7 @@ class Trainer:
             self,
             model: BaseModel,
             callbacks: Optional[Union[Callback, CallbackList, List[Callback]]] = None,
-            device: Optional[device] = None,
+            device: Optional[torch_device] = None,
             verbose: bool = True,
             **kwargs
     ):
@@ -109,32 +35,17 @@ class Trainer:
         self.sort_callbacks()
         self.device = self._set_default_device(device)
         self.verbose = verbose
-        self.state = TrainingState()
+
+        self.batch_state = BatchState()
+        self.batches_state = BatchesState()
+        self.epoch_state = EpochState()
+        self.training_state = TrainingState()
 
         self.x_transform = kwargs.get("x_transform", ToTensor())
         self.y_transform = kwargs.get("y_transform", ToTensor())
 
         self._checkpoint_loading_mode = None
         self._force_overwrite = None
-
-    @property
-    def model(self):
-        """
-        Alias for the model.
-
-        :return: The :attr:`model` attribute.
-        """
-        return self.model
-
-    @model.setter
-    def model(self, value):
-        """
-        Alias for the model.
-
-        :param value: The new value for the :attr:`model` attribute.
-        :return: None
-        """
-        self.model = value
 
     @property
     def checkpoint_loading_mode(self):
@@ -150,10 +61,14 @@ class Trainer:
 
     @property
     def training_history(self) -> TrainingHistory:
-        training_histories = list(filter(lambda x: isinstance(x, TrainingHistory), self.callbacks))[0]
+        training_histories = list(filter(lambda x: isinstance(x, TrainingHistory), self.callbacks))
         assert len(training_histories) == 1, "There should be one and only one 'TrainingHistory' callback."
 
         return training_histories[0]
+
+    @property
+    def learning_algorithms(self) -> CallbackList:
+        return CallbackList(list(filter(lambda x: isinstance(x, LearningAlgorithm), self.callbacks)))
 
     @property
     def model_checkpoint(self) -> Optional[ModelCheckpoint]:
@@ -165,7 +80,7 @@ class Trainer:
         else:
             return None
 
-    def _set_default_device(self, device: Optional[device]) -> device:
+    def _set_default_device(self, device: Optional[torch_device]) -> torch_device:
         if device is None:
             return self.model.device
         return device
@@ -181,30 +96,6 @@ class Trainer:
         if not any([isinstance(callback, TrainingHistory) for callback in callbacks]):
             callbacks.append(TrainingHistory())
         return CallbackList(callbacks)
-
-    def update_state(self, **kwargs):
-        self.state.update(**kwargs)
-
-    def update_objects_state(self, **kwargs):
-        self.update_state(objects={**self.state.objects, **kwargs})
-
-    def update_epoch_losses_and_metrics_state(self, **kwargs):
-        self.update_state(epoch_losses_and_metrics={**self.state.epoch_losses_and_metrics, **kwargs})
-
-    def update_info_state(self, **kwargs):
-        self.update_state(info={**self.state.info, **kwargs})
-
-    def update_train_losses(self, train_losses: Dict[str, float]):
-        self.state.epoch_losses_and_metrics[MaskType.TRAIN][MeasureCategory.LOSSES.value] = train_losses
-
-    def update_train_metrics(self, train_metrics: Dict[str, float]):
-        self.state.epoch_losses_and_metrics[MaskType.TRAIN][MeasureCategory.METRICS.value] = train_metrics
-
-    def update_valid_losses(self, valid_losses: Dict[str, float]):
-        self.state.epoch_losses_and_metrics[MaskType.VALID][MeasureCategory.LOSSES.value] = valid_losses
-
-    def update_valid_metrics(self, valid_metrics: Dict[str, float]):
-        self.state.epoch_losses_and_metrics[MaskType.VALID][MeasureCategory.METRICS.value] = valid_metrics
 
     def sort_callbacks(self):
         """
@@ -320,58 +211,55 @@ class Trainer:
         train_dataloader = self._create_train_dataloader(dataset=dataset, batch_size=batch_size)
         valid_dataloader = self._create_valid_dataloader(dataset=dataset)
 
-        self.update_state(
+        self.training_state.update(
             n_epochs=n_epochs,
-            objects={
-                **self.state.objects,
-                **{"train_dataloader": train_dataloader, "valid_dataloader": valid_dataloader, "tasks": dataset.tasks}
-            }
+            train_dataloader=train_dataloader,
+            valid_dataloader=valid_dataloader,
+            tasks=dataset.tasks
         )
 
         self.sort_callbacks()
         self.callbacks.on_fit_start(self)
 
         self.load_state()
-        if self.state.epoch is None:
-            self.update_state(epoch=0)
+        if self.epoch_state.idx is None:
+            self.epoch_state.idx = 0
         if len(self.training_history) > 0:
-            self.update_epoch_losses_and_metrics_state(**self.training_history[-1])
+            self.epoch_state.set_to_last_epoch_of_history(self.training_history)
 
         progress_bar = tqdm(
-            initial=self.state.epoch,
-            total=self.state.n_epochs,
+            initial=self.epoch_state.idx,
+            total=self.training_state.n_epochs,
             desc=kwargs.get("desc", "Training"),
             disable=not self.verbose,
             position=p_bar_position,
             unit="epoch",
             leave=p_bar_leave
         )
-        for epoch in self._epochs_generator(progress_bar):
-            self.update_state(epoch=epoch)
+        for epoch_idx in self._epochs_generator(progress_bar):
+            self.epoch_state.idx = epoch_idx
             self.callbacks.on_epoch_start(self)
-            train_dataloader = self.state.objects["train_dataloader"]
-            valid_dataloader = self.state.objects["valid_dataloader"]
+            train_dataloader = self.training_state.train_dataloader
+            valid_dataloader = self.training_state.valid_dataloader
 
-            epoch_loss = self._exec_epoch(train_dataloader, valid_dataloader)
+            self._exec_epoch(train_dataloader, valid_dataloader)
 
+            if exec_metrics_on_train or valid_dataloader:
+                self.model.fix_thresholds_to_optimal_values(dataset)
             if exec_metrics_on_train:
-                itr_train_metrics = self._exec_metrics(train_dataloader, prefix="train")
-            else:
-                itr_train_metrics = {}
-
+                self._exec_metrics(dataset=dataset, training=True)
             if valid_dataloader is not None:
-                itr_val_metrics = self._exec_metrics(valid_dataloader, prefix="val")
-            else:
-                itr_val_metrics = {}
+                self._exec_metrics(dataset=dataset, training=False)
 
             self.callbacks.on_epoch_end(self)
 
-            postfix = {f"{k}": f"{v:.5e}" for k, v in self.state.itr_metrics.items()}
+            postfix = self.epoch_state.as_dict()
             postfix.update(self.callbacks.on_progress_bar_update(self))
             progress_bar.set_postfix(postfix)
-            if self.state.stop_training_flag:
+            if self.training_state.stop_training_flag:
                 progress_bar.set_postfix(dict(**{"stop_flag": "True"}, **postfix))
                 break
+
         self.callbacks.on_fit_end(self)
         progress_bar.close()
 
@@ -383,89 +271,74 @@ class Trainer:
 
         :return: The current iteration.
         """
-        while self.state.epoch < self.state.n_epochs:
-            yield self.state.epoch
+        while self.epoch_state.idx < self.training_state.n_epochs:
+            yield self.epoch_state.idx
 
-            self.update_state(epoch=self.state.epoch + 1)
-            progress_bar.total = self.state.n_epochs
+            self.epoch_state.idx += 1
+            progress_bar.total = self.training_state.n_epochs
             progress_bar.update()
 
     def _exec_epoch(
             self,
             train_dataloader: DataLoader,
             valid_dataloader: Optional[DataLoader] = None
-    ) -> Dict[str, float]:
+    ):
         with no_grad():
             cuda.empty_cache()
-        losses = {}
 
-        train_loss = self._exec_training(train_dataloader)
-
-        losses["train_loss"] = train_loss
+        self._exec_training(train_dataloader)
 
         if valid_dataloader is not None:
             with no_grad():
-                val_loss = self._exec_validation(valid_dataloader)
-                losses["val_loss"] = val_loss
+                self._exec_validation(valid_dataloader)
 
         with no_grad():
             cuda.empty_cache()
 
-        return losses
-
-    def _exec_metrics(self, dataloader: DataLoader, prefix: str) -> Dict:
-        metrics_dict = {}
-        for metric in self.metrics:
-            m_out = metric(dataloader)
-            if isinstance(m_out, dict):
-                metrics_dict.update({f"{prefix}_{k}": v for k, v in m_out.items()})
-            else:
-                metric_name = str(metric)
-                if hasattr(metric, "name"):
-                    metric_name = metric.name
-                elif hasattr(metric, "__name__"):
-                    metric_name = metric.__name__
-                metrics_dict[f"{prefix}_{metric_name}"] = m_out
-        return metrics_dict
+    def _exec_metrics(
+            self,
+            dataset: ProstateCancerDataset,
+            training: bool
+    ):
+        if training:
+            scores = self.model.score_on_dataset(dataset=dataset, mask=dataset.train_mask)
+            self.epoch_state.train_single_task_metrics = scores
+        else:
+            scores = self.model.score_on_dataset(dataset=dataset, mask=dataset.valid_mask)
+            self.epoch_state.valid_single_task_metrics = scores
 
     def _exec_training(
             self,
             dataloader: DataLoader,
-    ) -> float:
+    ):
         self.model.train()
         self.callbacks.on_train_start(self)
-
-        train_loss = self._exec_batches(dataloader)
-
-        self.update_state_(train_loss=train_loss)
+        self._exec_batches(dataloader)
+        self.epoch_state.set_losses_from_batches_state(self.batches_state, self.model.training)
         self.callbacks.on_train_end(self)
-
-        return train_loss
 
     def _exec_validation(
             self,
             dataloader: DataLoader,
-    ) -> float:
+    ):
         self.model.eval()
         self.callbacks.on_validation_start(self)
-
-        val_loss = self._exec_batches(dataloader)
-
-        self.update_state_(val_loss=val_loss)
+        self._exec_batches(dataloader)
+        self.epoch_state.set_losses_from_batches_state(self.batches_state, self.model.training)
         self.callbacks.on_validation_end(self)
-
-        return val_loss
 
     def _exec_batches(
             self,
             dataloader: DataLoader
     ):
-        batch_losses = []
         for batch_idx, (x_batch, y_batch) in enumerate(dataloader):
-            self.update_state(batch=batch_idx)
-            batch_losses.append(to_numpy(self._exec_batch(x_batch, y_batch)))
+            self.batch_state.idx = batch_idx
+            self._exec_batch(x_batch, y_batch)
 
-        return np.nanmean(batch_losses)
+            if batch_idx == 0:
+                self.batches_state.init(self.batch_state)
+            else:
+                self.batches_state.append(self.batch_state)
 
     def _exec_batch(
             self,
@@ -474,7 +347,7 @@ class Trainer:
     ):
         x_batch = self.x_transform(self._batch_to_device(x_batch))
         y_batch = self.y_transform(self._batch_to_device(y_batch))
-        self.update_state(x_batch=x_batch, y_batch=y_batch)
+        self.batch_state.update(x=x_batch, y=y_batch)
 
         if self.model.training:
             self.callbacks.on_train_batch_start(self)
@@ -484,7 +357,7 @@ class Trainer:
             with no_grad():
                 pred_batch = self.model(x_batch)
 
-        self.update_state(pred_batch=pred_batch)
+        self.batch_state.pred = pred_batch
 
         if self.model.training:
             self.callbacks.on_optimization_start(self)
@@ -492,10 +365,6 @@ class Trainer:
             self.callbacks.on_train_batch_end(self)
         else:
             self.callbacks.on_validation_batch_end(self)
-
-        batch_loss = self.state.batch_loss
-
-        return batch_loss
 
     def _batch_to_device(
             self,
