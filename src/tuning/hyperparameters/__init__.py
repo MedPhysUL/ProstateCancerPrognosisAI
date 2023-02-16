09dcb05204ab2a0b84510b9ff3854cd02c52a060
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-02-16 13:06:53 -0500

    Update __init__.py

--- src/tuning/hyperparameters/__init__.py
+++ src/tuning/hyperparameters/__init__.py
@@ -2,3 +2,4 @@ from .categorical import CategoricalHyperparameter
 from .constant import ConstantHyperparameter
 from .float import FloatHyperparameter
 from .integer import IntegerHyperparameter
+from .object import ObjectHyperparameter
