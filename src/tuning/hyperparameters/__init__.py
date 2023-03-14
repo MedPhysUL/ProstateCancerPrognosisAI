5eca70f6841a66019e24d15ed1823de21a0580e0
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-03-14 07:40:27 -0400

    Remove now useless inits

--- src/tuning/hyperparameters/__init__.py
+++
@@ -1,6 +0,0 @@
-from .categorical import CategoricalHyperparameter
-from .fixed import FixedHyperparameter
-from .float import FloatHyperparameter
-from .integer import IntegerHyperparameter
-
-from .containers import HyperparameterDict, HyperparameterList, HyperparameterObject
