de2ad26f0ed84b5f08b0a950edbf30f3d047f86b
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-03-21 11:22:25 -0400

    Update binary_vs_prob.py

--- applications/binary_vs_prob.py
+++ applications/binary_vs_prob.py
@@ -1,20 +1,20 @@
 import torch
 
 
-# ----------- WHERE IS NOT DIFFERENTIABLE :( ----------- #
+# ----------- ROUND IS DIFFERENTIABLE BUT GIVES ALL 0 :( ----------- #
 a = torch.rand(3, 3, requires_grad=True)  # pixel values
 b = torch.rand(3, 3, requires_grad=True)  # label map as a probability map
 binary_b = torch.round(b)
-where_out = torch.where(binary_b == 1, a, 0.0)
-where_out = where_out.flatten()
-where_out = torch.nn.Linear(in_features=9, out_features=1)(where_out)
-where_out.backward()
+out = binary_b*a
+out = out.flatten()
+out = torch.nn.Linear(in_features=9, out_features=1)(out)
+out.backward()
 
 print(a.grad)
-print(b.grad)  # is None :(
+print(b.grad)  # is all 0
 
 
-# ----------- MUL IS DIFFERENTIABLE :) ----------- #
+# ----------- USE PROBABILITIES :) ----------- #
 a = torch.rand(3, 3, requires_grad=True)  # pixel values
 b = torch.rand(3, 3, requires_grad=True)  # label map as a probability map
 out = a*b
@@ -26,5 +26,5 @@ out.backward()
 print(a.grad)
 print(b.grad)
 
-# Conclusion : To make everything differentiable, multiply with the probability label map instead of using where with
-# the binary label map.
+# Conclusion : To make everything differentiable, multiply with the probability label map instead of using the binary
+# label map.
