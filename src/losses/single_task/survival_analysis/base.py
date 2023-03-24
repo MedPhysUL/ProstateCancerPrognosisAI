f23018297879fac3a886d4d94fcc611b478ba5ea
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-03-24 06:27:27 -0400

    Update base.py

--- src/losses/single_task/survival_analysis/base.py
+++ src/losses/single_task/survival_analysis/base.py
@@ -8,11 +8,9 @@
     @Description:       This file is used to define the abstract `SurvivalAnalysisLoss` class.
 """
 
-from abc import ABC, abstractmethod
-from typing import Tuple, Union
+from abc import ABC
+from typing import Union
 
-import numpy as np
-from torch import from_numpy, is_tensor, nan, tensor, Tensor
 
 from ..base import LossReduction
 from ..regression import RegressionLoss
@@ -42,82 +40,3 @@ class SurvivalAnalysisLoss(RegressionLoss, ABC):
         super().__init__(name=name, reduction=reduction)
 
         self.get_idx_of_nonmissing_targets = get_idx_of_nonmissing_survival_analysis_targets
-
-    def __call__(
-            self,
-            pred: Union[np.array, Tensor],
-            targets: Union[np.array, Tensor]
-    ) -> Tensor:
-        """
-        Converts inputs to tensors than computes the Loss and applies rounding.
-
-        Parameters
-        ----------
-        pred : Union[np.array, Tensor]
-            (N,) tensor or array with predicted labels.
-        targets : Union[np.array, Tensor]
-            (N,) tensor or array with ground truth
-
-        Returns
-        -------
-        loss : Tensor
-            Rounded loss score.
-        """
-        nonmissing_targets_idx = self.get_idx_of_nonmissing_targets(targets)
-        if len(nonmissing_targets_idx) == 0:
-            return tensor(nan, device=pred.device)
-
-        targets, pred = targets[nonmissing_targets_idx], pred[nonmissing_targets_idx]
-
-        if not is_tensor(pred):
-            pred, targets = self.convert_to_tensors(pred, targets)
-
-        return self.perform_reduction(self._compute_loss(pred, targets))
-
-    @staticmethod
-    def convert_to_tensors(
-            pred: Union[np.array, Tensor],
-            targets: Union[np.array, Tensor]
-    ) -> Tuple[Tensor, Tensor]:
-        """
-        Converts inputs to tensors.
-
-        Parameters
-        ----------
-        pred : Union[np.array, Tensor]
-            (N,) tensor or array containing predictions.
-        targets : Union[np.array, Tensor]
-            (N,) tensor or array containing ground truth.
-
-        Returns
-        -------
-        pred, targets : Tuple[Tensor, Tensor]
-            (N,) tensor, (N,) tensor
-        """
-        if not is_tensor(pred):
-            return from_numpy(pred).float(), from_numpy(targets).float()
-        else:
-            return pred, targets
-
-    @abstractmethod
-    def _compute_loss(
-            self,
-            pred: Tensor,
-            targets: Tensor
-    ) -> Tensor:
-        """
-        Computes the loss value.
-
-        Parameters
-        ----------
-        pred : Tensor
-            (N,) tensor with predicted labels
-        targets : Tensor
-            (N,) tensor with ground truth
-
-        Returns
-        -------
-        loss : Tensor
-            Loss.
-        """
-        raise NotImplementedError
