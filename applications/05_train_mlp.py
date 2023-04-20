fa305b9195bcf4cbbf4c36278910c434ba16ecbb
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-04-20 10:09:55 -0400

    Update 05_train_mlp.py

--- applications/05_train_mlp.py
+++ applications/05_train_mlp.py
@@ -90,6 +90,7 @@ if __name__ == '__main__':
     history.plot(show=True)
 
     # The next part will be integrated in an evaluation tool in the near future.
+    model.fix_thresholds_to_optimal_values(dataset)
     score = model.score_on_dataset(dataset, dataset.test_mask)
     print(score)
 
