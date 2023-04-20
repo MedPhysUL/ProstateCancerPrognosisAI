edfcd122d96b63c2da3d6f307537b2b79deee7dc
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-04-19 21:45:45 -0400

    Update 05_train_mlp.py

--- applications/05_train_mlp.py
+++ applications/05_train_mlp.py
@@ -2,32 +2,34 @@
     @file:              05_train_mlp.py
     @Author:            Maxence Larose
 
-    @Creation Date:     07/2022
-    @Last modification: 03/2023
+    @Creation Date:     04/2023
+    @Last modification: 04/2023
 
     @Description:       This script is used to train an mlp model.
 """
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ExponentialLR
 
 from constants import *
+from src.data.processing.sampling import extract_masks, Mask
 from src.data.datasets import ProstateCancerDataset, TableDataset
-from src.models.torch.prediction.tabular_mlp import TabularMLP
+from src.models.torch.prediction import MLP
 from src.losses.multi_task import MeanLoss
 from src.training import Trainer
 from src.training.callbacks import LearningAlgorithm, Checkpoint
-from src.training.callbacks.learning_algorithm import L2Regularizer
 from src.training.callbacks.learning_algorithm import MultiTaskLossEarlyStopper
+from src.training.callbacks.learning_algorithm.regularizer import L2Regularizer
 
 
 if __name__ == '__main__':
     df = pd.read_csv(LEARNING_TABLE_PATH)
 
     feature_cols = [AGE, PSA, GLEASON_GLOBAL, GLEASON_PRIMARY, GLEASON_SECONDARY, CLINICAL_STAGE]
-    target_cols = [PN, BCR, BCR_TIME]
+    target_cols = [PN, BCR, BCR_TIME, METASTASIS, METASTASIS_TIME, EE, SVI, CRPC, CRPC_TIME, DEATH, DEATH_TIME]
 
     df = df[[ID] + feature_cols + target_cols]
 
@@ -39,38 +41,44 @@ if __name__ == '__main__':
         cat_cols=[GLEASON_GLOBAL, GLEASON_PRIMARY, GLEASON_SECONDARY, CLINICAL_STAGE]
     )
 
-    dataset = ProstateCancerDataset(image_dataset=None, table_dataset=table_dataset)
+    dataset = ProstateCancerDataset(table_dataset=table_dataset)
+
+    masks = extract_masks(os.path.join(MASKS_PATH, "masks.json"), k=2, l=2)
 
     dataset.update_masks(
-        train_mask=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-        test_mask=[],
-        valid_mask=[20, 21, 22, 23, 24]
+        train_mask=masks[0][Mask.TRAIN],
+        test_mask=masks[0][Mask.TEST],
+        valid_mask=masks[0][Mask.VALID]
     )
 
-    model = TabularMLP(
+    model = MLP(
+        multi_task_mode="separated",
+        hidden_channels=[30, 30, 30],
         activation="PRELU",
-        hidden_channels=[20, 20, 20],
+        dropout=0.2,
         device=torch.device("cuda"),
         seed=SEED
     ).build(dataset)
 
     optimizer = Adam(
         params=model.parameters(),
-        lr=1e-2
+        lr=2e-4,
+        weight_decay=0.02
     )
 
     learning_algorithm = LearningAlgorithm(
         criterion=MeanLoss(),
         optimizer=optimizer,
-        lr_scheduler=ExponentialLR(optimizer=optimizer, gamma=0.999),
-        regularizer=L2Regularizer(params=model.named_parameters(), lambda_=0.01),
-        early_stopper=MultiTaskLossEarlyStopper()
+        lr_scheduler=ExponentialLR(optimizer=optimizer, gamma=0.99),
+        early_stopper=MultiTaskLossEarlyStopper(patience=20),
+        regularizer=L2Regularizer(model.named_parameters(), lambda_=0.01)
     )
+
     trainer = Trainer(
-        batch_size=16,
+        batch_size=8,
         checkpoint=Checkpoint(),
         exec_metrics_on_train=True,
-        n_epochs=30,
+        n_epochs=1,
         seed=SEED
     )
 
@@ -81,3 +89,29 @@ if __name__ == '__main__':
     )
 
     history.plot(show=True)
+
+    # The next part will be integrated in an evaluation tool in the near future.
+    score = model.score_on_dataset(dataset, dataset.test_mask)
+    print(score)
+
+    for task in dataset.tasks.survival_analysis_tasks:
+        breslow_estimator = task.breslow_estimator
+
+        cum_baseline_hazard = breslow_estimator.cum_baseline_hazard_
+        plt.plot(cum_baseline_hazard.x, cum_baseline_hazard.y)
+        plt.show()
+
+        baseline_survival = breslow_estimator.baseline_survival_
+        plt.plot(baseline_survival.x, baseline_survival.y)
+        plt.show()
+
+        prediction = trained_model.predict_on_dataset(dataset, [0, 1, 2, 3])[task.name].cpu()
+        chf_funcs = breslow_estimator.get_cumulative_hazard_function(prediction)
+        for fn in chf_funcs:
+            plt.step(fn.x, fn(fn.x), where="post")
+        plt.show()
+
+        chf_funcs = breslow_estimator.get_survival_function(prediction)
+        for fn in chf_funcs:
+            plt.step(fn.x, fn(fn.x), where="post")
+        plt.show()
