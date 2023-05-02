6706d72c8eb051a1e5274f1dbfd30e6640d3fcee
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-05-02 12:50:35 -0400

    Update 11_tune_multi_net.py

--- applications/11_tune_multi_net.py
+++ applications/11_tune_multi_net.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
 
     search_algo = SearchAlgorithm(
         sampler=TPESampler(
-            n_startup_trials=10,
+            n_startup_trials=5,
             multivariate=True,
             seed=SEED
         ),
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     tuner = Tuner(
         search_algorithm=search_algo,
         recorder=TuningRecorder(path_to_record_folder=path_to_record_folder),
-        n_trials=50,
+        n_trials=30,
         seed=SEED
     )
 
