b0ba405e3e3559ebc0f2eb78693b67496f7e33b2
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-05-12 23:29:48 -0400

    Add HTX task

--- applications/constants.py
+++ applications/constants.py
@@ -16,7 +16,7 @@ from src.metrics.single_task import (
 )
 from src.tasks import BinaryClassificationTask, SegmentationTask, SurvivalAnalysisTask
 from src.data.datasets import Feature
-from src.data.transforms import Normalization, OrdinalEncoding, MappingEncoding
+from src.data.transforms import Normalization, MappingEncoding
 
 # SEED
 SEED = 1010710
@@ -45,46 +45,33 @@ ID = "ID"
 
 # FEATURES
 AGE = Feature(column="AGE", transform=Normalization())
-PSA = Feature(column="PSA", transform=Normalization())
+CLINICAL_STAGE = Feature(column="CLINICAL_STAGE", transform=MappingEncoding({"T1-T2": 0, "T3a": 1}))
 GLEASON_GLOBAL = Feature(column="GLEASON_GLOBAL", transform=MappingEncoding({8: 0, 9: 0.5, 10: 1}))
 GLEASON_PRIMARY = Feature(column="GLEASON_PRIMARY", transform=MappingEncoding({3: 0, 4: 0.5, 5: 1}))
 GLEASON_SECONDARY = Feature(column="GLEASON_SECONDARY", transform=MappingEncoding({3: 0, 4: 0.5, 5: 1}))
-CLINICAL_STAGE = Feature(column="CLINICAL_STAGE", transform=MappingEncoding({"T1-T2": 0, "T3a": 1}))
+PSA = Feature(column="PSA", transform=Normalization())
 
 CONTINUOUS_FEATURES = [AGE, PSA]
-CATEGORICAL_FEATURES = [GLEASON_GLOBAL, GLEASON_PRIMARY, GLEASON_SECONDARY, CLINICAL_STAGE]
+CATEGORICAL_FEATURES = [CLINICAL_STAGE, GLEASON_GLOBAL, GLEASON_PRIMARY, GLEASON_SECONDARY]
 FEATURES = CONTINUOUS_FEATURES + CATEGORICAL_FEATURES
 
 # TARGETS
-PN = "PN"
 BCR = "BCR"
 BCR_TIME = "BCR_TIME"
-METASTASIS = "METASTASIS"
-METASTASIS_TIME = "METASTASIS_TIME"
-EE = "EE"
-SVI = "SVI"
 CRPC = "CRPC"
 CRPC_TIME = "CRPC_TIME"
 DEATH = "DEATH"
 DEATH_TIME = "DEATH_TIME"
+HTX = "HTX"
+HTX_TIME = "HTX_TIME"
+METASTASIS = "METASTASIS"
+METASTASIS_TIME = "METASTASIS_TIME"
+PN = "PN"
 
 # TABLE TASKS
-PN_TASK = BinaryClassificationTask(
-    target_column=PN,
-    decision_threshold_metric=BinaryBalancedAccuracy(),
-    hps_tuning_metric=AUC(),
-    evaluation_metrics=[Sensitivity(), Specificity()],
-    criterion=BCEWithLogitsLoss()
-)
 BCR_TASK = SurvivalAnalysisTask(
     event_indicator_column=BCR,
     event_time_column=BCR_TIME,
-    criterion=NegativePartialLogLikelihood(),
-    hps_tuning_metric=ConcordanceIndexCensored()
-)
-METASTASIS_TASK = SurvivalAnalysisTask(
-    event_indicator_column=METASTASIS,
-    event_time_column=METASTASIS_TIME,
     criterion=NegativePartialLogLikelihood(),
     hps_tuning_metric=ConcordanceIndexCensored()
 )
@@ -100,8 +87,27 @@ DEATH_TASK = SurvivalAnalysisTask(
     criterion=NegativePartialLogLikelihood(),
     hps_tuning_metric=ConcordanceIndexCensored()
 )
+HTX_TASK = SurvivalAnalysisTask(
+    event_indicator_column=HTX,
+    event_time_column=HTX_TIME,
+    criterion=NegativePartialLogLikelihood(),
+    hps_tuning_metric=ConcordanceIndexCensored()
+)
+METASTASIS_TASK = SurvivalAnalysisTask(
+    event_indicator_column=METASTASIS,
+    event_time_column=METASTASIS_TIME,
+    criterion=NegativePartialLogLikelihood(),
+    hps_tuning_metric=ConcordanceIndexCensored()
+)
+PN_TASK = BinaryClassificationTask(
+    target_column=PN,
+    decision_threshold_metric=BinaryBalancedAccuracy(),
+    hps_tuning_metric=AUC(),
+    evaluation_metrics=[Sensitivity(), Specificity()],
+    criterion=BCEWithLogitsLoss()
+)
 
-TABLE_TASKS = [BCR_TASK, PN_TASK, METASTASIS_TASK, CRPC_TASK, DEATH_TASK]
+TABLE_TASKS = [BCR_TASK, CRPC_TASK, DEATH_TASK, HTX_TASK, METASTASIS_TASK, PN_TASK]
 
 # IMAGE TASKS
 PROSTATE_SEGMENTATION_TASK = SegmentationTask(
