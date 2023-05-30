b01e85ccf5bcd9366f9e43fe9daba15eda671d95
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-05-30 14:44:09 -0400

    Use StratifiedKFold sampling instead of stratified random sampling

--- src/data/processing/sampling.py
+++ src/data/processing/sampling.py
@@ -10,11 +10,11 @@
 """
 
 from copy import deepcopy
-from typing import Any, Callable, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from torch import Tensor
 from tqdm import tqdm
 
@@ -33,10 +33,8 @@ class Sampler:
             n_out_split: int,
             n_in_split: int,
             valid_size: float = 0.20,
-            test_size: float = 0.20,
             random_state: Optional[int] = None,
-            alpha: int = 4,
-            patience: int = 100
+            alpha: int = 4
     ):
         """
         Sets private and public attributes of the sampler.
@@ -51,14 +49,10 @@ class Sampler:
             Number of inner splits to produce.
         valid_size : float
             Percentage of data taken to create the validation indexes set.
-        test_size : float
-            Percentage of data taken to create the test indexes set
         random_state : Optional[int]
             Random state integer.
         alpha : int
             IQR multiplier used to check numerical variable range validity of the masks created
-        patience : int
-            Number of tries that the sampler has to make a single valid split
         """
         if n_out_split <= 0:
             raise ValueError('Number of outer split must be greater than 0')
@@ -66,10 +60,6 @@ class Sampler:
             raise ValueError('Number of inner split must be greater or equal to 0')
         if not (0 <= valid_size < 1):
             raise ValueError('Validation size must be in the range [0, 1)')
-        if not (0 < test_size < 1):
-            raise ValueError('Test size must be in the range (0, 1)')
-        if valid_size + test_size >= 1:
-            raise ValueError('Train size must be non null')
 
         # Private attributes
         self.__dataset = dataset
@@ -90,11 +80,11 @@ class Sampler:
         self.alpha = alpha
         self.n_out_split = n_out_split
         self.n_in_split = n_in_split
-        self.patience = patience
         self.random_state = random_state
+        self.valid_size = valid_size
 
-        # Public method
-        self.split = self.__define_split_function(test_size, valid_size)
+        self.outer_skf = StratifiedKFold(n_splits=n_out_split, shuffle=True, random_state=random_state)
+        self.inner_skf = StratifiedKFold(n_splits=n_in_split, shuffle=True, random_state=random_state)
 
     def __call__(
             self
@@ -130,10 +120,6 @@ class Sampler:
         # We set targets to use for stratification
         targets = [target if self.is_categorical(target) else self.mimic_classes(target) for target in self.__targets]
 
-        # We set the random state
-        if self.random_state is not None:
-            np.random.seed(self.random_state)
-
         # We initialize the dict that will contain the results and the list of indexes to use
         masks, idx = {}, np.array(range(len(targets[0])))
 
@@ -141,16 +127,30 @@ class Sampler:
         targets_c = deepcopy(targets)
 
         with tqdm(total=(self.n_out_split + self.n_out_split*self.n_in_split)) as bar:
-            for i in range(self.n_out_split):
+            targets_str = targets_c[0].astype(str)
+            for i in range(len(targets_c) - 1):
+                targets_str = np.char.add(targets_str, targets_c[i + 1].astype(str))
+
+            outer_splits = self.outer_skf.split(X=idx, y=targets_str[idx])
+            for i, (outer_remaining_idx, outer_test_mask) in enumerate(outer_splits):
+
+                outer_train_mask, outer_valid_mask = self._get_train_valid_masks(outer_remaining_idx, targets_str)
 
                 # We create outer split masks
-                masks[i] = {**self.split(idx, targets_c), Mask.INNER: {}}
+                masks[i] = {
+                    **{Mask.TRAIN: outer_train_mask, Mask.VALID: outer_valid_mask, Mask.TEST: outer_test_mask},
+                    Mask.INNER: {}
+                }
                 bar.update()
 
-                for j in range(self.n_in_split):
+                inner_splits = self.inner_skf.split(X=outer_train_mask, y=targets_str[outer_train_mask])
+                for j, (inner_remaining_idx, inner_test_mask) in enumerate(inner_splits):
+                    inner_train_mask, inner_valid_mask = self._get_train_valid_masks(inner_remaining_idx, targets_str)
 
                     # We create the inner split masks
-                    masks[i][Mask.INNER][j] = self.split(masks[i][Mask.TRAIN], targets_c)
+                    masks[i][Mask.INNER][j] = {
+                        Mask.TRAIN: inner_train_mask, Mask.VALID: inner_valid_mask, Mask.TEST: inner_test_mask
+                    }
                     bar.update()
 
         # We turn arrays of idx into lists of idx
@@ -158,134 +158,31 @@ class Sampler:
 
         return masks
 
-    def __define_split_function(
+    def _get_train_valid_masks(
             self,
-            test_size: float,
-            valid_size: float
-    ) -> Callable:
+            remaining_idx: np.ndarray,
+            targets_str: np.ndarray
+    ) -> Tuple[np.ndarray, Union[np.ndarray, List[int]]]:
         """
-        Defines the split function according to the valid size.
+        Returns lists of indexes to use as train and valid masks. The proportion of each class is conserved within
+        each split.
 
         Parameters
         ----------
-        test_size : float
-            Percentage of data taken to create the train indexes set.
-        valid_size : float
-            Percentage of data taken to create the validation indexes set.
-
-        Returns
-        -------
-        split : Callable
-            Split function.
+        remaining_idx : np.ndarray
+            Array of indexes to split.
+        targets_str : Union[np.ndarray, List[int]]
+            Array of targets used for stratification.
         """
-
-        if valid_size > 0:
-
-            # Split must extract train, valid and test masks
-            def split(idx: np.array, targets: List[np.array]) -> Dict[str, np.array]:
-                train_mask, valid_mask, test_mask = None, None, None
-                # We make a pseudo-multilabel
-                targets_str = targets[0].astype(str)
-                for i in range(len(targets)-1):
-                    targets_str = np.char.add(targets_str, targets[i+1].astype(str))
-
-                # We initialize loop important values
-                mask_ok = False
-                nb_tries_remaining = self.patience
-
-                # We test multiple possibilities till we find one or the patience is achieved
-                while not mask_ok and nb_tries_remaining > 0:
-                    remaining_idx, test_mask = train_test_split(idx, stratify=targets_str[idx], test_size=test_size)
-                    train_mask, valid_mask = train_test_split(remaining_idx, stratify=targets_str[remaining_idx],
-                                                              test_size=valid_size)
-                    mask_ok = self.check_masks_validity(train_mask, test_mask, valid_mask)
-                    nb_tries_remaining -= 1
-
-                if not mask_ok:
-                    raise Exception("The sampler could not find a proper train, valid and test split.")
-
-                return {Mask.TRAIN: train_mask, Mask.VALID: valid_mask, Mask.TEST: test_mask}
+        if np.isclose(self.valid_size, 0.0):
+            return remaining_idx, []
         else:
-            # Split must extract train and test masks only
-            def split(idx: np.array, targets: List[np.array]) -> Dict[str, np.array]:
-                train_mask, test_mask = None, None
-                # We make a pseudo-multilabel
-                targets_str = targets[0].astype(str)
-                for i in range(len(targets)-1):
-                    targets_str = np.char.add(targets_str, targets[i+1].astype(str))
-
-                # We initialize loop important values
-                mask_ok = False
-                nb_tries_remaining = self.patience
-
-                # We test multiple possibilities till we find one or the patience is achieved
-                while not mask_ok and nb_tries_remaining > 0:
-                    train_mask, test_mask = train_test_split(idx, stratify=targets_str[idx], test_size=test_size)
-                    mask_ok = self.check_masks_validity(train_mask, test_mask)
-                    nb_tries_remaining -= 1
-
-                if not mask_ok:
-                    raise Exception("The sampler could not find a proper train and test split")
-
-                return {Mask.TRAIN: train_mask, Mask.VALID: None, Mask.TEST: test_mask}
-
-        return split
-
-    def check_masks_validity(
-            self,
-            train_mask: List[int],
-            test_mask: List[int],
-            valid_mask: Optional[List[int]] = None
-    ) -> bool:
-        """
-        Valid if categorical and numerical variables of other masks are out of the range of train mask.
-
-        Parameters
-        ----------
-        train_mask : List[int]
-            List of idx to use for training.
-        test_mask : List[int]
-            List of idx to use for test.
-        valid_mask : Optional[List[int]]
-            List of idx to use for validation.
-
-        Returns
-        -------
-        valid : bool
-            Whether the masks are valid.
-        """
-        # We update the masks of the dataset
-        self.__dataset.update_masks(train_mask, test_mask, valid_mask)
-
-        # We extract train dataframe
-        imputed_df = self.__dataset.imputed_df
-        train_df = imputed_df.iloc[train_mask]
-
-        # We check if all categories of categorical columns are in the training set
-        for cat, values in self.__unique_encodings.items():
-            for c in train_df[cat].unique():
-                if c not in values:
-                    return False
-
-        # We save q1 and q3 of each numerical columns
-        train_quantiles = {c: (train_df[c].quantile(0.25), train_df[c].quantile(0.75))
-                           for c in self.__dataset.cont_features_cols}
-
-        # We validate the other masks
-        other_masks = [m for m in [valid_mask, test_mask] if m is not None]
-        for mask in other_masks:
-
-            # We extract the subset
-            subset_df = imputed_df.iloc[mask]
-
-            # We check if all numerical values are not extreme outliers according to the train mask
-            for cont_col, (q1, q3) in train_quantiles.items():
-                iqr = q3 - q1
-                other_min, other_max = (subset_df[cont_col].min(), subset_df[cont_col].max())
-                if other_min < q1 - self.alpha * iqr or other_max > q3 + self.alpha * iqr:
-                    return False
-
-            return True
+            return train_test_split(
+                remaining_idx,
+                stratify=targets_str[remaining_idx],
+                test_size=self.valid_size,
+                random_state=self.random_state
+            )
 
     @staticmethod
     def is_categorical(
