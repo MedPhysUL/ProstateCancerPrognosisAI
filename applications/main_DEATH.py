bf57eda85c3a7ca39759b80b478120ad41cf6eca
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-06-11 12:38:00 -0400

    Migrating from MultiNet to CNN and removing docker path fix

--- applications/main_DEATH.py
+++ applications/main_DEATH.py
@@ -10,16 +10,11 @@ from constants import *
 from src.data.datasets import ImageDataset, ProstateCancerDataset, TableDataset
 from src.data.processing.sampling import extract_masks
 from src.losses.multi_task import MeanLoss
-from src.models.torch import CNN, MLP, MultiNet
-from src.models.torch.combination import ModelSetup
+from src.models.torch import CNN
 from src.training.callbacks.learning_algorithm import L2Regularizer, MultiTaskLossEarlyStopper
 from src.tuning import SearchAlgorithm, TorchObjective, Tuner
 from src.tuning.callbacks import TuningRecorder
 
-from src.tuning.hyperparameters.containers import (
-    HyperparameterList,
-    HyperparameterObject
-)
 from src.tuning.hyperparameters.optuna import (
     CategoricalHyperparameter,
     FixedHyperparameter,
@@ -40,33 +35,27 @@ from src.tuning.hyperparameters.torch import (
 
 
 if __name__ == '__main__':
-    DOCKER_EXPERIMENTS_PATH = "experiments"
-    task = DEATH_TASK
-
     df = pd.read_csv(LEARNING_TABLE_PATH)
+    task = DEATH_TASK
 
     table_dataset = TableDataset(
         df=df,
         ids_col=ID,
-        tasks=task,
-        cont_features=CONTINUOUS_FEATURES,
-        cat_features=CATEGORICAL_FEATURES
+        tasks=task
     )
 
     database = PatientsDatabase(path_to_database=r"local_data/learning_set.h5")
 
     image_dataset = ImageDataset(
         database=database,
-        modalities={"PT", "CT"},
-        organs={"CT": {"Prostate"}}
+        modalities={"PT", "CT"}
     )
 
     dataset = ProstateCancerDataset(image_dataset=image_dataset, table_dataset=table_dataset)
 
     path_to_record_folder = os.path.join(
-        os.getcwd(),
-        DOCKER_EXPERIMENTS_PATH,
-        f"{task.target_column}(MultiNet - Clinical data + Deep radiomics)"
+        EXPERIMENTS_PATH,
+        f"{task.target_column}(CNN - Deep radiomics)"
     )
 
     search_algo = SearchAlgorithm(
@@ -85,81 +74,16 @@ if __name__ == '__main__':
     )
 
     model_hyperparameter = TorchModelHyperparameter(
-        constructor=MultiNet,
+        constructor=CNN,
         parameters={
-            "predictor_setup": HyperparameterObject(
-                constructor=ModelSetup,
-                parameters={
-                    "model": TorchModelHyperparameter(
-                        constructor=MLP,
-                        parameters={
-                            "activation": FixedHyperparameter(name="activation", value="PReLU"),
-                            "input_mode": "hybrid",
-                            "multi_task_mode": "separated",
-                            "hidden_channels": CategoricalHyperparameter(
-                                name="hidden_channels",
-                                choices=["(20, 20, 20)", "(30, 30, 30)", "(40, 40, 40)"]
-                            ),
-                            "dropout": FloatHyperparameter(name="dropout_mlp", low=0.05, high=0.25)
-                        }
-                    )
-                }
-            ),
-            "extractor_setup": HyperparameterObject(
-                constructor=ModelSetup,
-                parameters={
-                    "model": TorchModelHyperparameter(
-                        constructor=CNN,
-                        parameters={
-                            "image_keys": ["CT", "PT"],
-                            "segmentation_key_or_task": "CT_Prostate",
-                            "model_mode": "extraction",
-                            "merging_method": "multiplication",
-                            "multi_task_mode": "separated",
-                            "channels": CategoricalHyperparameter(
-                                name="channels",
-                                choices=[
-                                    "(8, 16, 32, 64, 128)", "(16, 32, 64, 128, 256)",
-                                    "(32, 64, 128, 256, 512)", "(64, 128, 256, 512, 1024)"
-                                ]
-                            ),
-                            "kernel_size": FixedHyperparameter(name="kernel_size", value=3),
-                            "num_res_units": FixedHyperparameter(name="num_res_units", value=3),
-                            "dropout_cnn": FloatHyperparameter(name="dropout_extractor_cnn", low=0.1, high=0.7),
-                            "dropout_fnn": FloatHyperparameter(name="dropout_extractor_fnn", low=0.1, high=0.7)
-                        }
-                    ),
-                }
-            )
+                    "image_keys": ["CT", "PT"],
+                    "model_mode": "prediction",
+                    "channels": FixedHyperparameter(name="channels", value=(64, 128, 256, 512, 1024)),
+                    "kernel_size": FixedHyperparameter(name="kernel_size", value=3),
+                    "num_res_units": FixedHyperparameter(name="num_res_units", value=3),
+                    "dropout_cnn": FloatHyperparameter(name="dropout_cnn", low=0.2, high=0.8),
+                    "dropout_fnn": FloatHyperparameter(name="dropout_fnn", low=0.1, high=0.4)
         }
-    )
-
-    predictor_learning_algorithm_hyperparameter = LearningAlgorithmHyperparameter(
-        criterion=CriterionHyperparameter(
-            constructor=MeanLoss
-        ),
-        optimizer=OptimizerHyperparameter(
-            constructor=Adam,
-            model_params_getter=lambda model: model.predictor.parameters(),
-            parameters={
-                "lr": FloatHyperparameter(name="lr_predictor", low=1e-4, high=1e-2, log=True),
-                "weight_decay": FloatHyperparameter(name="weight_decay_predictor", low=1e-4, high=1e-2, log=True)
-            }
-        ),
-        clip_grad_max_norm=PREDICTOR_CLIP_GRAD_MAX_NORM_DICT[task.target_column],
-        early_stopper=EarlyStopperHyperparameter(
-            constructor=MultiTaskLossEarlyStopper,
-            parameters={"patience": 20}
-        ),
-        lr_scheduler=LRSchedulerHyperparameter(
-            constructor=ExponentialLR,
-            parameters={"gamma": FixedHyperparameter(name="gamma_predictor", value=0.99)}
-        ),
-        regularizer=RegularizerHyperparameter(
-            constructor=L2Regularizer,
-            model_params_getter=lambda model: model.predictor.named_parameters(),
-            parameters={"lambda_": FloatHyperparameter(name="alpha_predictor", low=1e-5, high=1e-2, log=True)}
-        )
     )
 
     extractor_learning_algorithm_hyperparameter = LearningAlgorithmHyperparameter(
@@ -170,8 +94,8 @@ if __name__ == '__main__':
             constructor=Adam,
             model_params_getter=lambda model: model.extractor.parameters(),
             parameters={
-                "lr": FloatHyperparameter(name="lr_extractor", low=1e-5, high=1e-2, log=True),
-                "weight_decay": FloatHyperparameter(name="weight_decay_extractor", low=1e-4, high=1e-2, log=True)
+                "lr": FloatHyperparameter(name="lr", low=1e-5, high=1e-3, log=True),
+                "weight_decay": FloatHyperparameter(name="weight_decay", low=1e-3, high=1e-1, log=True)
             }
         ),
         clip_grad_max_norm=EXTRACTOR_CLIP_GRAD_MAX_NORM_DICT[task.target_column],
@@ -181,12 +105,12 @@ if __name__ == '__main__':
         ),
         lr_scheduler=LRSchedulerHyperparameter(
             constructor=ExponentialLR,
-            parameters={"gamma": FixedHyperparameter(name="gamma_extractor", value=0.99)}
+            parameters={"gamma": FixedHyperparameter(name="gamma", value=0.95)}
         ),
         regularizer=RegularizerHyperparameter(
             constructor=L2Regularizer,
             model_params_getter=lambda model: model.extractor.named_parameters(),
-            parameters={"lambda_": FloatHyperparameter(name="alpha_extractor", low=1e-6, high=1e-3, log=True)}
+            parameters={"lambda_": FloatHyperparameter(name="alpha", low=1e-4, high=1e-2, log=True)}
         )
     )
 
@@ -199,9 +123,7 @@ if __name__ == '__main__':
 
     train_method_hyperparameter = TrainMethodHyperparameter(
         model=model_hyperparameter,
-        learning_algorithms=HyperparameterList(
-            [predictor_learning_algorithm_hyperparameter, extractor_learning_algorithm_hyperparameter]
-        )
+        learning_algorithms=extractor_learning_algorithm_hyperparameter
     )
 
     objective = TorchObjective(
