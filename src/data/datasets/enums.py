bc5e76da665e6b5a1640cd26bbad6d2ec8ac5ffc
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-06-26 15:50:06 -0400

    Refractoring mask for enums

--- src/data/datasets/enums.py
+++ src/data/datasets/enums.py
@@ -1,11 +1,11 @@
 """
-    @file:              mask.py
+    @file:              enums.py
     @Author:            Maxence Larose, Nicolas Raymond, Raphael Brodeur
 
     @Creation Date:     05/2022
-    @Last modification: 02/2023
+    @Last modification: 06/2023
 
-    @Description:       This file contains the 'Mask' enum class.
+    @Description:       This file contains the 'Mask' and 'Split' enum class.
 """
 
 from enum import auto, StrEnum
@@ -19,3 +19,11 @@ class Mask(StrEnum):
     VALID = auto()
     TEST = auto()
     INNER = auto()
+
+
+class Split(StrEnum):
+    """
+    Stores the constant related to split categories.
+    """
+    INNER = auto()
+    OUTER = auto()
