1b747d380133b3320fa35fbf25e9ce9abc111dd4
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-06-28 14:02:51 -0400

    Update table.py

--- src/data/datasets/table.py
+++ src/data/datasets/table.py
@@ -106,6 +106,7 @@ class TableDataset(Dataset):
         self._ids_col = ids_column
         self._imputed_df = None
         self._imputer_features_cols = [f.column for f in self.features if f.impute is True]
+        self._imputer_cat_features_cols = list(set(self._cat_features_cols).intersection(self._imputer_features_cols))
         self._iterative_imputer = IterativeImputer(
             estimator=RandomForestRegressor(random_state=random_state),
             tol=1e-2,
@@ -648,7 +649,7 @@ class TableDataset(Dataset):
         data = self._iterative_imputer.transform(df)
         temp_df = pd.DataFrame(data, columns=self._imputer_features_cols)
 
-        for column in self._cat_features_cols:
+        for column in self._imputer_cat_features_cols:
             original_array = np.array(df[column].dropna())
             categories = np.unique(original_array)
             bins = self._convert_categories_to_bins(categories)
