bug fixe with saving average shap values

src/explainer/shap_explainer.py
@@ -326,11 +326,6 @@ class TableShapValueExplainer:
                     for target in targets
                 }
 
-            if path_to_save_folder is not None:
-                path_to_save_folder = os.path.join(
-                    path_to_save_folder,
-                    f"{kwargs.get('filename', 'average_shap_values.pdf')}"
-                )
             title = kwargs.get("title", "Average Feature Importances")
             axis_title = kwargs.get("axis", "Features")
             x_pos = (np.arange(len(feature_names)))
@@ -340,9 +335,9 @@ class TableShapValueExplainer:
             arr.set_xlabel(axis_title)
             arr.set_title(title)
 
-            file_name = 'average_shap_plot.pdf' if not absolute else 'average_absolute_shap_plot.pdf'
             if path_to_save_folder is not None:
                 target_names = self.dataset.table_dataset.target_columns
+                file_name = 'average_absolute_shap_plot.pdf' if absolute else 'average_shap_plot.pdf'
                 path = os.path.join(
                     path_to_save_folder,
                     f"{kwargs.get('target', target_names[target])}_{kwargs.get('filename', file_name)}"