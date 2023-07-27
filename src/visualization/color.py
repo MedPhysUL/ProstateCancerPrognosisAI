04c7ea0b2dbdc44a70023bc438a5e341e4ff083a
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-07-27 08:37:02 -0400

    Create color.py

---
+++ src/visualization/color.py
@@ -0,0 +1,38 @@
+"""
+    @file:              color.py
+    @Author:            Maxence Larose
+
+    @Creation Date:     07/2023
+    @Last modification: 07/2023
+
+    @Description:       This file contains the Color enum class.
+"""
+
+from enum import StrEnum
+
+
+class LightColor(StrEnum):
+    BLUE = "#9DC9E2"
+    GREEN = "#B2DAAC"
+    RED = "#FFA99C"
+    PINK = "#CC87B3"
+    PURPLE = "#9C99C6"
+    SKIN = "#FECFA1"
+
+
+class Color(StrEnum):
+    BLUE = "#70A7C7"
+    GREEN = "#87C07E"
+    RED = "#D87363"
+    PINK = "#AF598F"
+    PURPLE = "#716DA8"
+    SKIN = "#F1B88C"
+
+
+class DarkColor(StrEnum):
+    BLUE = "#4F609C"
+    GREEN = "#226258"
+    RED = "#9D343C"
+    PINK = "#A1437E"
+    PURPLE = "#4C3264"
+    SKIN = "#B39171"
