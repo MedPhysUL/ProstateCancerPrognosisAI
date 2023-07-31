0d229a6173d00e3cd4a7fe102798fd1ce37faf59
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-07-31 15:48:49 -0400

    Update color.py

--- src/visualization/color.py
+++ src/visualization/color.py
@@ -13,26 +13,26 @@ from enum import StrEnum
 
 class LightColor(StrEnum):
     BLUE = "#9DC9E2"
+    SAND = "#FECFA1"
     GREEN = "#B2DAAC"
     RED = "#FFA99C"
     PINK = "#CC87B3"
     PURPLE = "#9C99C6"
-    SAND = "#FECFA1"
 
 
 class Color(StrEnum):
     BLUE = "#70A7C7"
+    SAND = "#F1B88C"
     GREEN = "#87C07E"
     RED = "#D87363"
     PINK = "#AF598F"
     PURPLE = "#716DA8"
-    SAND = "#F1B88C"
 
 
 class DarkColor(StrEnum):
     BLUE = "#4F609C"
+    SAND = "#B39171"
     GREEN = "#226258"
     RED = "#9D343C"
     PINK = "#A1437E"
     PURPLE = "#4C3264"
-    SAND = "#B39171"
