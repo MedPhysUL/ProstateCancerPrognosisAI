d2f4b3d79301bb24c9a03b6a812a53e0371ee85e
FelixDesroches <117761543+FelixDesroches@users.noreply.github.com>
2023-08-15 13:16:09 -0400

    added testing file (félix)

--- applications/interpreter_tests.py
+++ applications/interpreter_tests.py
@@ -9,6 +9,7 @@
 """
 
 import env_apps
+import copy
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -21,13 +22,15 @@ from src.data.processing.sampling import extract_masks, Mask
 from src.data.datasets import ProstateCancerDataset, TableDataset
 from src.evaluation import ModelEvaluator
 from src.explainer.shap_explainer import TableShapValueExplainer, CaptumWrapper
-from src.explainer.sage_explainer import TableSageValueExplainer, SageWrapper
+from src.explainer.survshap_explainer import  TableSurvshapExplainer
 from src.models.torch.prediction import MLP
 from src.losses.multi_task import MeanLoss
 from src.training import Trainer
 from src.training.callbacks import LearningAlgorithm, Checkpoint
 from src.training.callbacks.learning_algorithm import MultiTaskLossEarlyStopper
 from src.training.callbacks.learning_algorithm.regularizer import L2Regularizer
+
+from src.evaluation.model_comparator import ModelComparator
 
 
 if __name__ == '__main__':
@@ -37,8 +40,8 @@ if __name__ == '__main__':
         dataframe=df,
         ids_column=ID,
         tasks=TABLE_TASKS,
-        continuous_features=CONTINUOUS_FEATURES,
-        categorical_features=CATEGORICAL_FEATURES
+        continuous_features=CLINICAL_CONTINUOUS_FEATURES,
+        categorical_features=CLINICAL_CATEGORICAL_FEATURES
     )
 
     dataset = ProstateCancerDataset(table_dataset=table_dataset)
@@ -58,6 +61,7 @@ if __name__ == '__main__':
         device=torch.device("cpu"),
         seed=SEED
     ).build(dataset)
+    model_backup = copy.deepcopy(model)
 
     learning_algorithms = []
     for task in TABLE_TASKS:
@@ -96,15 +100,18 @@ if __name__ == '__main__':
     # history.plot(show=True)
 
     show = True
-
-    # sage_explainer = TableSageValueExplainer(model=model, dataset=dataset, imputer='marginal', values=dataset.test_mask)
-    # sage_explainer.plot_sage_values_by_permutation(masks[(len(masks)//3):(2*len(masks)//3)], target=0, show=show)
-    # sage_explainer.plot_sage_values_by_iteration(masks[(len(masks) // 3):(2 * len(masks) // 3)], target=0, show=show)
-
-    shap_explainer = TableShapValueExplainer(model=model, dataset=dataset)
-    shap_explainer.plot_force(targets=0, patient_id=0, show=show)
-    shap_explainer.plot_waterfall(targets=0, patient_id=0, show=show)
-    shap_explainer.plot_beeswarm(targets=0, show=show)
-    shap_explainer.plot_bar(targets=0, show=show)
-    shap_explainer.plot_scatter(targets=[0, 1, 2, 3], show=show)
-    print(shap_explainer.compute_average_shap_values(targets=[0, 1, 2, 3, 4], absolute=True, show=show))
+    explainer = TableShapValueExplainer(model, dataset)
+    explainer.plot_waterfall(0, 0)
+    explainer.plot_beeswarm(0)
+    # explainer = TableSurvshapExplainer(model=trained_model, dataset=dataset)
+    # explainer.plot_shap_average_of_absolute_value(features={("PSA", "AGE"): [0, 1, 2, 3, 4, 4]}, function='sf', normalize=True, show=True, tasks=BCR_TASK, path_to_save_folder=None)
+    # explainer.plot_shap_average_of_absolute_value(features={("PSA", "AGE"): [0, 1, 2, 3, 4, 4]}, function='sf', normalize=False, show=True, tasks=BCR_TASK, path_to_save_folder=None)
+    # explainer.plot_shap_lines_for_all_patients(features={("PSA", "AGE"): [0, 1, 2, 3, 4, 4]}, function='sf', normalize=True, show=True, tasks=BCR_TASK, path_to_save_folder=None)
+    # explainer.plot_shap_lines_for_features(features={("PSA", "AGE"): [0, 1, 2, 3, 4, 4]}, function='sf', normalize=True, show=True, tasks=BCR_TASK, path_to_save_folder=None)
+    # explainer.plot_shap_lines_for_all_patients(features={("PSA", "AGE"): [0, 1, 2, 3, 4, 4]}, function='sf', normalize=False, show=True, tasks=BCR_TASK, path_to_save_folder=None)
+    # explainer.plot_shap_lines_for_features(features={("PSA", "AGE"): [0, 1, 2, 3, 4, 4]}, function='sf', normalize=False, show=True, tasks=BCR_TASK,  path_to_save_folder=None)
+    # comparator = ModelComparator(model_backup, trained_model, dataset)
+    # print(comparator.compute_c_index_p_value())
+    # print(comparator.compute_auc_p_value())
+    # print(comparator.compute_variance_p_value())
+    # print(comparator.compute_mean_p_value())
