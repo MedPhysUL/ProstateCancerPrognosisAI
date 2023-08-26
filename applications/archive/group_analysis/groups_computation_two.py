38da14af773802c6aac7b653b940d875e9cb9152
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2023-08-26 10:56:50 -0400

    Create groups_computation_two.py

---
+++ applications/archive/group_analysis/groups_computation_two.py
@@ -0,0 +1,175 @@
+import env_apps
+
+import json
+import os
+
+import pandas as pd
+import torch
+
+from constants import (
+    CLINICAL_CATEGORICAL_FEATURES,
+    CLINICAL_CONTINUOUS_FEATURES,
+    EXPERIMENTS_PATH,
+    FINAL_BAYES_TABLE_PATH,
+    HOLDOUT_MASKS_PATH,
+    ID,
+    SEED,
+    BCR_TASK,
+    CRPC_TASK,
+    PN_TASK,
+    METASTASIS_TASK,
+    HTX_TASK,
+    DEATH_TASK,
+    TABLE_TASKS
+)
+from src.data.datasets import Feature
+from src.data.transforms import Normalization
+from src.data.processing.sampling import Mask
+from src.data.datasets import ProstateCancerDataset, TableDataset
+from src.visualization.table import TableViewer
+from src.models.torch.prediction import SequentialNet
+
+import numpy as np
+from sksurv.compare import compare_survival
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(FINAL_BAYES_TABLE_PATH)
+
+    RADIOMIC_1 = Feature(column="RADIOMIC_PN_1", transform=Normalization(), impute=False)
+    RADIOMIC_2 = Feature(column="RADIOMIC_PN_2", transform=Normalization(), impute=False)
+    RADIOMIC_3 = Feature(column="RADIOMIC_PN_3", transform=Normalization(), impute=False)
+    RADIOMIC_4 = Feature(column="RADIOMIC_PN_4", transform=Normalization(), impute=False)
+    RADIOMIC_5 = Feature(column="RADIOMIC_PN_5", transform=Normalization(), impute=False)
+    RADIOMIC_6 = Feature(column="RADIOMIC_PN_6", transform=Normalization(), impute=False)
+    RADIOMIC_7 = Feature(column="RADIOMIC_BCR_1", transform=Normalization(), impute=False)
+    RADIOMIC_8 = Feature(column="RADIOMIC_BCR_2", transform=Normalization(), impute=False)
+    RADIOMIC_9 = Feature(column="RADIOMIC_BCR_3", transform=Normalization(), impute=False)
+    RADIOMIC_10 = Feature(column="RADIOMIC_BCR_4", transform=Normalization(), impute=False)
+    RADIOMIC_11 = Feature(column="RADIOMIC_BCR_5", transform=Normalization(), impute=False)
+    RADIOMIC_12 = Feature(column="RADIOMIC_BCR_6", transform=Normalization(), impute=False)
+
+    PN_RADIOMICS = [RADIOMIC_1, RADIOMIC_2, RADIOMIC_3, RADIOMIC_4, RADIOMIC_5, RADIOMIC_6]
+    BCR_RADIOMICS = [RADIOMIC_7, RADIOMIC_8, RADIOMIC_9, RADIOMIC_10, RADIOMIC_11, RADIOMIC_12]
+
+    RADIOMICS = PN_RADIOMICS + BCR_RADIOMICS
+
+    table_dataset = TableDataset(
+        dataframe=df,
+        ids_column=ID,
+        tasks=[PN_TASK, BCR_TASK, METASTASIS_TASK, HTX_TASK, CRPC_TASK, DEATH_TASK],
+        continuous_features=CLINICAL_CONTINUOUS_FEATURES + RADIOMICS,
+        categorical_features=CLINICAL_CATEGORICAL_FEATURES
+    )
+
+    dataset = ProstateCancerDataset(table_dataset=table_dataset)
+
+    masks = json.load(open(HOLDOUT_MASKS_PATH, "r"))
+
+    dataset.update_masks(
+        train_mask=masks[Mask.TRAIN],
+        test_mask=masks[Mask.TEST],
+        valid_mask=masks[Mask.VALID]
+    )
+
+    cont_features = [c.column for c in CLINICAL_CONTINUOUS_FEATURES]
+    cat_features = [c.column for c in CLINICAL_CATEGORICAL_FEATURES]
+    model = SequentialNet(
+        sequence=[PN_TASK.name, BCR_TASK.name, METASTASIS_TASK.name, HTX_TASK.name, CRPC_TASK.name, DEATH_TASK.name],
+        n_layers={PN_TASK.name: 2, BCR_TASK.name: 1, METASTASIS_TASK.name: 0, HTX_TASK.name: 2, CRPC_TASK.name: 1, DEATH_TASK.name: 2},
+        n_neurons={PN_TASK.name: 10, BCR_TASK.name: 15, METASTASIS_TASK.name: 0, HTX_TASK.name: 10, CRPC_TASK.name: 10, DEATH_TASK.name: 10},
+        features_columns={
+            PN_TASK.name: cont_features + [c.column for c in PN_RADIOMICS] + cat_features,
+            BCR_TASK.name: cont_features + [c.column for c in BCR_RADIOMICS] + cat_features,
+            METASTASIS_TASK.name: cont_features + cat_features,
+            HTX_TASK.name: cont_features + cat_features,
+            CRPC_TASK.name: cont_features + cat_features,
+            DEATH_TASK.name: cont_features + cat_features
+        },
+        dropout={
+            PN_TASK.name: 0,
+            BCR_TASK.name: 0,
+            METASTASIS_TASK.name: 0,
+            HTX_TASK.name: 0,
+            CRPC_TASK.name: 0,
+            DEATH_TASK.name: 0
+        },
+        bayesian=True,
+        temperature={
+            PN_TASK.name: 0.0001,
+            BCR_TASK.name: 0.001,
+            METASTASIS_TASK.name: 0.0001,
+            HTX_TASK.name: 0.0001,
+            CRPC_TASK.name: 0.001,
+            DEATH_TASK.name: 0.001
+        },
+        device=torch.device("cuda"),
+        seed=SEED
+    ).build(dataset)
+
+    state = torch.load(
+        os.path.join(
+            EXPERIMENTS_PATH,
+            r"HOLDOUT\DEATH(BayesSeqNet - Clinical data only)\best_model_checkpoint.pt"
+        )
+    )["model_state"]
+
+    model.load_state_dict(state)
+    model.fix_thresholds_to_optimal_values(dataset)
+    model.fit_breslow_estimators(dataset)
+
+    score = model.compute_score_on_dataset(dataset, dataset.train_mask, 100)
+    print(score)
+    score = model.compute_score_on_dataset(dataset, dataset.valid_mask, 100)
+    print(score)
+    score = model.compute_score_on_dataset(dataset, dataset.test_mask, 100)
+    print(score)
+
+    for task in [BCR_TASK, METASTASIS_TASK, HTX_TASK, CRPC_TASK, DEATH_TASK]:
+        df = dataset.table_dataset.dataframe.iloc[list(range(250))]
+        df_copy = df.copy()
+        df_copy.dropna(subset=[task.event_indicator_column, task.event_time_column], inplace=True)
+
+        value_counts = df[task.target_column].value_counts()
+        ratio_0 = value_counts[0] / (value_counts[0] + value_counts[1])
+        round_ratio = int(round(ratio_0, 2)*100)
+        if round_ratio > 95:
+            round_ratio = 90
+
+        print(f"{task.target_column} - ratio", round_ratio)
+        percentiles = np.array(list(range(round_ratio - 20, min(round_ratio + 20, 95))))
+
+        rows_not_dropped_indices = df_copy.index.tolist()
+        pred = model.predict_on_dataset(dataset=dataset, mask=rows_not_dropped_indices, n_samples=100)
+
+        p_values = []
+        thresholds = []
+        for percentile in percentiles:
+            prediction = pred[task.name]
+            prediction = prediction.cpu().detach().numpy()[:, 0]
+
+            threshold_value = np.percentile(prediction, percentile)
+            below_threshold = np.where(prediction < threshold_value)[0]
+            above_threshold = np.where(prediction >= threshold_value)[0]
+
+            final_group_indices = np.zeros_like(prediction, dtype=int)
+            final_group_indices[above_threshold] = 1
+
+            _, p_value = compare_survival(
+                y=TableViewer._get_structured_array(
+                    event_indicator=df_copy[task.event_indicator_column],
+                    event_time=df_copy[task.event_time_column]
+                ),
+                group_indicator=final_group_indices
+            )
+            p_values.append(p_value)
+            thresholds.append(threshold_value)
+
+        p_values = np.array(p_values)
+        p_valus_neighbor_means = (p_values[:-2] + p_values[1:-1] + p_values[2:]) / 3.0
+
+        print(f"{task.target_column} - p_value", p_values[np.argmin(p_valus_neighbor_means)])
+        best_threshold = thresholds[np.argmin(p_valus_neighbor_means)]
+        print(f"{task.target_column} - threshold", best_threshold)
+        best_percentile = percentiles[np.argmin(p_valus_neighbor_means)]
+        print(f"{task.target_column} - percentile", best_percentile)
